6514866a6fd3497b81ab68ad2b82da902fdb5f49
V.Kondratyev <V.Kondratyev@nedra.digital>
2021-04-19 10:24:40 +0300

    CVMTC-225 Add upc status check before campaign end

--- src/main/java/com/x5/bigdata/dvcm/process/dto/CampaignStatusDto.java
+++ src/main/java/com/x5/bigdata/dvcm/process/dto/CampaignStatusDto.java
@@ -6,12 +6,14 @@ import com.x5.bigdata.dvcm.process.entity.CampaignStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
+@ToString
 public class CampaignStatusDto {
     @JsonProperty("camp_id")
     private String campaignCode;
